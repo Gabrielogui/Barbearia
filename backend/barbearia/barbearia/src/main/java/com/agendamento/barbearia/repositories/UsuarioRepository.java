deaaf50cebf7c80f350366f0666303c551d93f39
Gabrielogui <gabrielcsro@gmail.com>
2025-04-08 15:55:34 -0300

    Criando as classes de controle, service e repositorio do usuário
    > UsuarioController.java
    > UsuarioRepository.java
    > UsuarioService.java

---
+++ backend/barbearia/barbearia/src/main/java/com/agendamento/barbearia/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.agendamento.barbearia.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.agendamento.barbearia.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+}
---
+++ backend/barbearia/barbearia/src/main/java/com/agendamento/barbearia/services/UsuarioService.java
@@ -0,0 +1,8 @@
+package com.agendamento.barbearia.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    
+}
