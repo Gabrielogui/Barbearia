e6d94e84b51eba005267fee05100706f0cf2fdae
Gabrielogui <gabrielcsro@gmail.com>
2025-04-08 13:49:43 -0300

    Configurando 'application.properties' + banco de dados
    Estabelecendo a conxeão com o banco de dados postgresql, previamente criado + teste criando a classe 'Usuario'

---
+++ backend/barbearia/barbearia/src/main/java/com/agendamento/barbearia/entity/Usuario.java
@@ -0,0 +1,26 @@
+package com.agendamento.barbearia.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_usuario")
+public class Usuario {
+    // |=======| ATRIBUTOS |=======|
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String role;  // "CLIENTE" || "PROFISSIONAL"
+    
+}
